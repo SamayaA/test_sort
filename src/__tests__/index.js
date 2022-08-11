f7b1b7e834f27edbf1deb975077cb130a429a412
SamayaA <asadullaevasamaya@gmail.com>
2022-08-11 20:08:35 +0300

    Test sort characters by health

---
+++ src/__tests__/index.js
@@ -0,0 +1,32 @@
+// test(description, work);
+import { sortCharacters } from '../index';
+
+test('sort characters by health - toBe', () => {
+  const characters = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedCharacters = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(sortCharacters(characters)).toBe(sortedCharacters);
+});
+
+test('sort characters by health - toEqual', () => {
+  const characters = [
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+  ];
+
+  const sortedCharacters = [
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
+  ];
+  expect(sortCharacters(characters)).toEqual(sortedCharacters);
+});
